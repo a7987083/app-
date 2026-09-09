92e4bce2ce69ba9239379aaea65d87f73a9c5900
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-10 03:26:18 +0800

    Delete routes directory

--- routes/theme.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Web\InstallController;
-use App\Http\Controllers\Web\ThemeController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [ThemeController::class, 'index']);
-
-Route::get('install', [InstallController::class, 'index']);
