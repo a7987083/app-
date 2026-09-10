17a390835a990fcffce044536a62f6052ddd782c
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-11 01:26:28 +0800

    fix: harden dylib lookup and remove category N+1 queries

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,111 +4,155 @@ namespace app\index\controller;
 
 use app\common\controller\Frontend;
 use think\Db;
+
 class Index extends Frontend
 {
-
     protected $noNeedLogin = '*';
     protected $noNeedRight = '*';
     protected $layout = '';
-    
-    public function dylib(){
+
+    public function dylib()
+    {
         header('Content-Type: application/json;charset=utf-8');
-        $arr = ['code'=>0, 'msg'=>'未获取设备UDID！', 'data'=>[]];
-        if(empty($_REQUEST['udid'])){
-            echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
+        $arr = ['code' => 0, 'msg' => '未获取设备UDID！', 'data' => []];
+        if (empty($_REQUEST['udid'])) {
+            echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+            die;
         }
-        $config = Db::table('fa_config')->select();
-        foreach ($config as $key=>$val)
-        {
-            if($val['name'] == 'name') $arr['data']['name'] = $val['value'];
-            if($val['name'] == 'payURL') $arr['data']['pay'] = $val['value'];
-            if($val['name'] == 'dylib-look') $arr['data']['look'] = $val['value'];
-            if($val['name'] == 'dylib-control') $arr['data']['control'] = $val['value'];
-            if($val['name'] == 'dylib-notice') $arr['data']['notice'] = $val['value'];
-            if($val['name'] == 'dylib-time') $arr['data']['time'] = $val['value'];
-            if($val['name'] == 'dylib-on') $arr['data']['on'] = $val['value'];
-        }
-        $udid =  $_REQUEST['udid'];
-        $res = DB::name('kami')->where('udid',$udid)->find();
-        $black = DB::name('black')->where('udid',$res['udid'])->find();
-        if(!$res){
+
+        $arr['data'] = $this->dylibConfig();
+        $udid = $_REQUEST['udid'];
+        $res = Db::name('kami')->where('udid', $udid)->find();
+        if (!$res) {
             $arr['msg'] = '未查到解锁记录！';
             $arr['code'] = 2;
-            echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
+            echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+            die;
         }
-        if($res['endtime'] > time()){
-            if ($black) {
-                $arr['msg'] = 'UDID黑名单';
-                $arr['code'] = 666;
-                echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
-            }
-            $arr['msg'] = '验证成功';
-            $arr['code'] = 1;
-            echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
-        }else{
-            $res = DB::name('kami')->where('udid',$udid)->where(['endtime'=>['>',time()]])->find();
-            if(!$res){
+
+        $black = Db::name('black')->where('udid', $res['udid'])->find();
+        if ($res['endtime'] <= time()) {
+            $res = Db::name('kami')->where('udid', $udid)->where(['endtime' => ['>', time()]])->find();
+            if (!$res) {
                 $arr['msg'] = '设备解锁已到期！';
                 $arr['code'] = 3;
-                echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
-            }else{
-                if ($black) {
-                    $arr['msg'] = 'UDID黑名单';
-                    $arr['code'] = 666;
-                    echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
-                }
-                $arr['msg'] = '验证成功';
-                $arr['code'] = 1;
-                echo json_encode($arr,JSON_UNESCAPED_UNICODE);die;
+                echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+                die;
             }
         }
-    }
-    public function apiface(){
-        if(empty($_REQUEST['udid'])){
-            echo json_encode(['msg'=>'未获取设备udid'],JSON_UNESCAPED_UNICODE);die;
+
+        if ($black) {
+            $arr['msg'] = 'UDID黑名单';
+            $arr['code'] = 666;
+        } else {
+            $arr['msg'] = '验证成功';
+            $arr['code'] = 1;
         }
-        $udid =  $_REQUEST['udid'];
-        $res = DB::name('kami')->where('udid',$udid)->find();
-        if(!$res){
-            echo json_encode(['msg'=>'未查到解锁记录'],JSON_UNESCAPED_UNICODE);die;
-        }
-        $endtime = $res['endtime'];
-        if($endtime > time()){
-            echo json_encode(['msg'=>'ok'],JSON_UNESCAPED_UNICODE);die;
-        }else{
-            $time = time();
-            $res = DB::name('kami')->where('udid',$udid)->where(['endtime'=>['>',$time]])->find();
-            if(!$res){
-                echo json_encode(['msg'=>'解锁已到期'],JSON_UNESCAPED_UNICODE);die;
-            }else{
-                echo json_encode(['msg'=>'ok'],JSON_UNESCAPED_UNICODE);die;
-            }
-            
-        }
-    }
-    public function index()
-    {
-    	if(!empty($_POST['uid'])){
-    		$id = (int)$_POST['uid'];
-    		$time = (int)date('d',time());
-    		$s = DB::name('category')->where('id',$id)->where('cstime',$time)->find();
-    		if($s){
-    			DB::name('category')->where('id',$id)->setInc('cs');
-    		}else{
-    			DB::name('category')->where('id',$id)->update([
-    				'cs' => 1,
-    				'cstime' => $time
-    			]);
-    		}
-    		return 'ok';
-    	}
-    	$data['img'] = DB::name('attachment')->whereNotNull('urls')->select();
-    	$data['category'] = DB::name('category')->where('pid',0)->where('status','normal')->order('weigh desc')->select();
-    	$data['xm'] = [];
-    	foreach ($data['category'] as $v){
-    		$data['xm'][$v['id']] = DB::name('category')->where('pid',$v['id'])->where('status','normal')->order('weigh desc')->select();
-    	}
-        return $this->view->fetch('',$data);
+        echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+        die;
     }
 
+    public function apiface()
+    {
+        if (empty($_REQUEST['udid'])) {
+            echo json_encode(['msg' => '未获取设备udid'], JSON_UNESCAPED_UNICODE);
+            die;
+        }
+
+        $udid = $_REQUEST['udid'];
+        $res = Db::name('kami')->where('udid', $udid)->find();
+        if (!$res) {
+            echo json_encode(['msg' => '未查到解锁记录'], JSON_UNESCAPED_UNICODE);
+            die;
+        }
+
+        if ($res['endtime'] <= time()) {
+            $res = Db::name('kami')->where('udid', $udid)->where(['endtime' => ['>', time()]])->find();
+            if (!$res) {
+                echo json_encode(['msg' => '解锁已到期'], JSON_UNESCAPED_UNICODE);
+                die;
+            }
+        }
+
+        echo json_encode(['msg' => 'ok'], JSON_UNESCAPED_UNICODE);
+        die;
+    }
+
+    public function index()
+    {
+        if (!empty($_POST['uid'])) {
+            $id = (int)$_POST['uid'];
+            $time = (int)date('d', time());
+            $s = Db::name('category')->where('id', $id)->where('cstime', $time)->find();
+            if ($s) {
+                Db::name('category')->where('id', $id)->setInc('cs');
+            } else {
+                Db::name('category')->where('id', $id)->update([
+                    'cs' => 1,
+                    'cstime' => $time,
+                ]);
+            }
+            return 'ok';
+        }
+
+        $data['img'] = Db::name('attachment')->whereNotNull('urls')->select();
+        $data['category'] = Db::name('category')
+            ->where('pid', 0)
+            ->where('status', 'normal')
+            ->order('weigh desc')
+            ->select();
+        $data['xm'] = $this->loadChildrenByParent($data['category']);
+
+        return $this->view->fetch('', $data);
+    }
+
+    protected function dylibConfig()
+    {
+        $map = [
+            'name' => 'name',
+            'payURL' => 'pay',
+            'dylib-look' => 'look',
+            'dylib-control' => 'control',
+            'dylib-notice' => 'notice',
+            'dylib-time' => 'time',
+            'dylib-on' => 'on',
+        ];
+        $data = [];
+        $config = Db::table('fa_config')->select();
+        foreach ($config as $row) {
+            if (isset($row['name']) && isset($map[$row['name']])) {
+                $data[$map[$row['name']]] = $row['value'];
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Replace one query per parent with one child query while preserving
+     * each parent's weigh-desc order and the original xm[parentId] shape.
+     */
+    protected function loadChildrenByParent($parents)
+    {
+        $grouped = [];
+        $parentIds = [];
+        foreach ($parents as $parent) {
+            $parentIds[] = $parent['id'];
+            $grouped[$parent['id']] = [];
+        }
+        if (!$parentIds) {
+            return $grouped;
+        }
+
+        $children = Db::name('category')
+            ->where('pid', 'in', $parentIds)
+            ->where('status', 'normal')
+            ->order('weigh desc')
+            ->select();
+        foreach ($children as $child) {
+            if (isset($grouped[$child['pid']])) {
+                $grouped[$child['pid']][] = $child;
+            }
+        }
+        return $grouped;
+    }
 }
