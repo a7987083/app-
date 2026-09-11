25ccb2e8d2616a096ff7e86f42e5f164c312ea19
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-11 08:22:32 +0800

    phase10: expose self-service unbind route

--- application/route.php
+++ application/route.php
@@ -12,17 +12,10 @@
 use think\Route;
 Route::rule('appstore','index/App/list');
 Route::rule('log','index/App/log');
+Route::rule('unbind','index/Index/unbind');
 return [
-    //别名配置,别名只能是映射到控制器且访问时必须加上请求的方法
     '__alias__'   => [
     ],
-    //变量规则
     '__pattern__' => [
     ],
-//        域名绑定到模块
-//        '__domain__'  => [
-//            'admin' => 'admin',
-//            'api'   => 'api',
-//        ],
-//Route::rule('appstore','index/App/list');
 ];
