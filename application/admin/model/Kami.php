6dd7ece6f6adfcfe16e08cf90c38ca9bc13a7487
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-17 08:05:54 +0800

    fix: clean app mappings when cards are deleted

--- application/admin/model/Kami.php
+++ application/admin/model/Kami.php
@@ -2,18 +2,16 @@
 
 namespace app\admin\model;
 
+use think\Db;
 use think\Model;
 
 
 class Kami extends Model
 {
 
-    
-
-    
     // 表名
     protected $name = 'kami';
-    
+
     // 自动写入时间戳字段
     protected $autoWriteTimestamp = false;
 
@@ -28,11 +26,18 @@ class Kami extends Model
         'usetime_text',
         'endtime_text'
     ];
-    
 
-    
-
-
+    protected static function init()
+    {
+        // fa_kami_app 是“指定 App 卡 -> App”的附属映射。
+        // 卡密仅到期时不会触发 delete，因此映射会保留；只有真正删除卡密时才同步删除。
+        self::beforeDelete(function ($row) {
+            $id = isset($row['id']) ? (int)$row['id'] : 0;
+            if ($id > 0) {
+                Db::table('fa_kami_app')->where('kami_id', $id)->delete();
+            }
+        });
+    }
 
     public function getAddtimeTextAttr($value, $data)
     {
@@ -40,13 +45,11 @@ class Kami extends Model
         return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
     }
 
-
     public function getUsetimeTextAttr($value, $data)
     {
         $value = $value ? $value : (isset($data['usetime']) ? $data['usetime'] : '');
         return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
     }
-
 
     public function getEndtimeTextAttr($value, $data)
     {
@@ -68,6 +71,5 @@ class Kami extends Model
     {
         return $value === '' ? 0 : ($value && !is_numeric($value) ? strtotime($value) : $value);
     }
-
 
 }
